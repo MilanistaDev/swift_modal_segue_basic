e57aae2d80c77123c2a25d7a249a8ff98fabb25f
MilanistaDev <milanista224@gmail.com>
2014-12-03 02:47:15 +0900

    Initial Commit

---
+++ swift_modal_segue_basic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift_modal_segue_basic
+//
+//  Created by 麻生 拓弥 on 2014/12/03.
+//  Copyright (c) 2014年 Takuya Aso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
